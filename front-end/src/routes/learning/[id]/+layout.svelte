09fce97cbbab23b9085bb9ae4813c970b505adad
egnaro1007 <ndviettuan@gmai.com>
2024-12-09 20:18:02 +0700

    Move the dino and board to page.svelte

--- front-end/src/routes/learning/[id]/+layout.svelte
+++ front-end/src/routes/learning/[id]/+layout.svelte
@@ -1,12 +1,5 @@
 <div class="arrange">
     <div class="container">
-		<div class="arrange__board">
-			  	<slot>{children}</slot>
-        </div>
-        <img src="/images/khunglong.png" alt="khunglong" class="arrange__dinosaur" />
-        <div class="arrange__scoreboard centered">
-            <p>Số điểm</p>
-            <p>0</p>
-        </div>
+		<slot>{children}</slot>
     </div>
 </div>
--- front-end/src/routes/learning/[id]/+page.svelte
+++ front-end/src/routes/learning/[id]/+page.svelte
@@ -1,127 +1,80 @@
 <script>
     import { page } from '$app/stores';
-    import Loading from '$lib/components/Loading.svelte';
+	import Loading from '$lib/components/Loading.svelte';
+    import FillInTheBlank from '$lib/components/question-types/FillInTheBlank.svelte';
+    import SentenceRearrangement from '$lib/components/question-types/SentenceRearrangement.svelte';
+    import Translation from '$lib/components/question-types/Translation.svelte';
 
     const learningID = $page.params.id;
     let questions = [];
     let currentIndex = 0;
     let loading = true;
-    let selectedAnswers = [];
 
     async function fetchQuestions() {
         loading = true;
-        await new Promise((resolve) => setTimeout(resolve, 500));
+        await new Promise(resolve => setTimeout(resolve, 1000));
         questions = [
             {
-                questionID: 1,
-                type: 'rearrangement',
-                question: 'Sắp xếp câu',
-                answerList: ['câu', 'Sắp xếp', 'có nghĩa', 'từ', 'thành']
+              questionID: 1,
+              description: 'Sắp xếp từ thành câu',
+              type: 'rearrangement',
+              question: 'Sắp xếp câu',
+              answerList: [
+                'video:https://qipedc.moet.gov.vn/videos/W00442.mp4',
+                'video:https://qipedc.moet.gov.vn/videos/W02931T.mp4',
+                'video:https://qipedc.moet.gov.vn/videos/W03712.mp4',
+                'video:https://qipedc.moet.gov.vn/videos/W00559.mp4'
+              ],
+              key: [
+                'video:https://qipedc.moet.gov.vn/videos/W02931T.mp4',
+                'video:https://qipedc.moet.gov.vn/videos/W03712.mp4',
+                'video:https://qipedc.moet.gov.vn/videos/W00559.mp4',
+                'video:https://qipedc.moet.gov.vn/videos/W00442.mp4'
+              ]
             },
-            {
-                questionID: 2,
-                type: 'rearrangement',
-                question: 'Sắp xếp câu',
-                answerList: ['đậu', 'Sắp xếp', 'có nghĩa', 'từ', 'thành', 'sẵn sàng']
-            },
-            {
-                questionID: 3,
-                type: 'rearrangement',
-                question: 'Sắp xếp câu',
-                answerList: ['cây', 'Sắp xếp', 'có nghĩa', 'từ', 'thành']
-            }
+            { questionID: 2, description: 'Sắp xếp từ thành câu có nghĩa', type: 'rearrangement', question: 'Sắp xếp câu', answerList: ['câu', 'Sắp xếp', 'có nghĩa', 'từ', 'thành'] },
+            { questionID: 3, type: 'cloze', question: 'Điền từ vào chỗ trống', answerList: ['Câu hỏi' , 'điền', ,'vào', 'chỗ trống'] },
+            { questionID: 4, type: 'translation', question: 'Dịch từ này', answerList: ['Từ cần dịch'] },
+            { questionID: 5, type: 'translation'}
         ];
-        selectedAnswers = new Array(questions[currentIndex].answerList.length).fill(null);
         loading = false;
     }
 
     function nextQuestion() {
         currentIndex = Math.min(currentIndex + 1, questions.length - 1);
-        selectedAnswers = new Array(questions[currentIndex].answerList.length).fill(null);
     }
 
     function previousQuestion() {
         currentIndex = Math.max(currentIndex - 1, 0);
-        selectedAnswers = new Array(questions[currentIndex].answerList.length).fill(null);
     }
-
-    function selectAnswer(index) {
-		console.log(index);
-        const answer = questions[currentIndex].answerList[index];
-        const emptyIndex = selectedAnswers.indexOf(null);
-        if (emptyIndex !== -1) {
-            selectedAnswers[emptyIndex] = answer;
-            questions[currentIndex].answerList[index] = null;
-        }
-    }
-
-    function returnAnswer(index) {
-        const answer = selectedAnswers[index];
-        if (answer !== null) {
-            const emptyIndex = questions[currentIndex].answerList.indexOf(null);
-            if (emptyIndex !== -1) {
-                questions[currentIndex].answerList[emptyIndex] = answer;
-                selectedAnswers[index] = null;
-            }
-        }
-    }
-
     fetchQuestions();
 </script>
 
 <div>
-    {#if loading}
-        <div
-            style="display: flex; flex-direction: column; justify-content: center; align-items: center; height: 539.4px;"
-        >
+    <div class="arrange__board">
+        {#if loading}
             <Loading />
-        </div>
-    {:else}
-        <div class="arrange__time">
-            <h3>Thời gian còn lại:</h3>
-            <span>10</span>
-        </div>
-        <div class="arrange__count">
-            <h3>Câu hỏi:</h3>
-            <span>{currentIndex + 1}/{questions.length}</span>
-        </div>
-
-        <!-- Selected Answers (Holders) -->
-        <div class="arrange__answer">
-            <ul class="arrange__items">
-                {#each selectedAnswers as answer, index}
-                    <li
-                        class="{answer === null ? 'arrange__holder' : 'arrange__item'}"
-                        on:click={() => returnAnswer(index)}
-                    >
-                        {answer}
-                    </li>
-                {/each}
-            </ul>
-        </div>
-
-        <!-- Submit Button -->
-        <div class="arrange__button">
-            <button class="arrange__submit">Xác nhận</button>
-        </div>
-
-        <!-- Choices -->
-        <div class="arrange__choices">
-            <ul class="arrange__items">
-                {#each questions[currentIndex].answerList as answer, index}
-                    <li
-                        class="{answer === null ? 'arrange__holder' : 'arrange__item'}"
-                        on:click={() => selectAnswer(index)}
-                    >
-                        {answer}
-                    </li>
-                {/each}
-            </ul>
-        </div>
-
-        <!-- Navigation Buttons -->
-        <button on:click={previousQuestion} disabled={currentIndex === 0}>Previous</button>
-        <button on:click={nextQuestion} disabled={currentIndex === questions.length - 1}>Next</button>
-    {/if}
+        {:else}
+            <div class="arrange__count">
+                <h3>Câu hỏi:</h3>
+                <span>{currentIndex + 1}/{questions.length}</span>
+            </div>
+            
+            {#if questions[currentIndex].type === 'cloze'}
+                <FillInTheBlank questionData={ questions[currentIndex] } />
+            {:else if questions[currentIndex].type === 'rearrangement'}
+                <SentenceRearrangement questionData={ questions[currentIndex] } />
+            {:else if questions[currentIndex].type === 'translation'}
+                <Translation questionData={ questions[currentIndex] } />
+            {/if}
+        
+            <button on:click={previousQuestion} disabled={currentIndex === 0}>Previous</button>
+            <button on:click={nextQuestion} disabled={currentIndex === questions.length - 1}>Next</button>
+        {/if}
+    </div>
+    <img src="/images/khunglong.png" alt="khunglong" class="arrange__dinosaur" />
+    <div class="arrange__scoreboard centered">
+        <p>Số điểm</p>
+        <p>0</p>
+    </div>
 </div>
-
