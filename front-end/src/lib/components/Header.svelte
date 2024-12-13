b6487c3886cbe0174d897f5a267f91a6b6d1cbf1
ndtuananh04 <125666050+ndtuananh04@users.noreply.github.com>
2024-12-13 21:23:30 +0700

    minor change

--- front-end/src/lib/components/Header.svelte
+++ front-end/src/lib/components/Header.svelte
@@ -1,5 +1,8 @@
 <script>
 	export let searchPlaceholder = 'Tìm kiếm chủ đề, từ vựng...';
+	import {page} from '$app/stores';
+	let sign;
+	$: sign = $page.params.id;
 </script>
 
 <header class="header">
@@ -8,9 +11,16 @@
 			<a href="/"> <img src="/images/logo.png" alt="Logo" /></a>
 			<input type="text" placeholder={searchPlaceholder} style="" />
 		</div>
+		{#if sign}
+			<div class="header__right">
+				<h4>Nguyễn Đình Tuấn Anh</h4>
+				<img src="/images/userz.png" alt="user" >
+			</div>
+		{:else}
 		<div class="header__right">
 			<h4>Đăng nhập</h4>
 			<h4>Đăng ký</h4>
 		</div>
+		{/if}
 	</div>
 </header>
