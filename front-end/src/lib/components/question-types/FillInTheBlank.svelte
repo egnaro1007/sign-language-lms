d2ec7c0e2ba0c2ccd8a527c7fb3f8567c5518092
egnaro1007 <ndviettuan@gmai.com>
2024-12-13 22:04:34 +0700

    Add camera

--- front-end/src/lib/components/question-types/FillInTheBlank.svelte
+++ front-end/src/lib/components/question-types/FillInTheBlank.svelte
@@ -1,8 +1,11 @@
 <script>
+    import { onMount } from "svelte";
 	export let questionData = {};
 	export let finishQuestion = () => {};
 	let isChecked = false;
 	let userInput = '';
+	let videoElement;
+	let cameraAccessAllowed = false;
 
 	$: userInput, checkAnswer();
 
@@ -18,6 +21,61 @@
 		}
 	}
 
+	async function captureFrameAndSend() {
+		if (!cameraAccessAllowed) {
+			return;
+		}
+
+		const canvas = document.createElement('canvas');
+		canvas.width = videoElement.videoWidth;
+		canvas.height = videoElement.videoHeight;
+		const context = canvas.getContext('2d');
+		context.drawImage(videoElement, 0, 0, canvas.width, canvas.height);
+	
+		  canvas.toBlob(async (blob) => {
+			if (!blob) {
+			console.error('Failed to create Blob');
+			return;
+			}
+	
+			try {
+				const formData = new FormData();
+				formData.append('file', blob, 'frame.jpg');
+			
+				const response = await fetch('http://127.0.0.1:8001/api/detect', {
+					method: 'POST',
+					body: formData
+				});
+			
+				const result = await response.json();
+				if (result) {
+					userInput = result.sign_language;
+				}
+			} catch (err) {
+				console.error('Error sending frame:', err);
+			}
+		  }, 'image/jpeg');
+	}
+
+	onMount(async () => {
+			try {
+				const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+				videoElement.srcObject = stream;
+				cameraAccessAllowed = true;
+
+				setInterval(() => {
+					cameraAccessAllowed = (videoElement.srcObject && videoElement.srcObject.active);
+					if (cameraAccessAllowed) {
+						captureFrameAndSend();
+					}
+					console.log(userInput);
+				}, 1000);
+			
+			} catch (err) {
+				console.error('Error accessing camera: ', err);
+			}
+  		}
+	);
 </script>
 
 <div>
@@ -39,12 +97,19 @@
                                     <source src={item.slice(6)} type="video/mp4">
                                     <track kind="captions" srclang="vi" label="Vietnamese" style="display: none;">
                                 </video>
-							{:else if item.startsWith('input:')}
-								<div class="learning__item">
-									<textarea bind:value={userInput} style="width: 100%; height: 100%;"></textarea>
-								</div>
-                            {:else}
+                            {:else if item.startsWith('text:')}
                                 {item}
+							{:else}
+								<!-- <div class="learning__item">
+									<textarea bind:value={userInput} style="width: 100%; height: 100%;"></textarea>
+								</div> -->
+                                <div class="learning__camera">
+                                    <!-- <textarea bind:value={userInput} style="width: 100%; height: 100%;"></textarea> -->
+                                    <video bind:this={videoElement} autoplay style="width: 100%; height: 100%; transform: scaleX(-1);">
+                                        <track kind="captions" />
+                                    </video>
+                                    <p>{userInput}</p>
+                                </div>                               
                             {/if}
                         </li>
                     {/each}
